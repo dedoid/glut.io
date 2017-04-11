0e95e6986953a19eb9a26b998db881a359d48bb5
dedoid <bot.sedoid@gmail.com>
2017-04-11 03:04:16 -0400

    Initial commit

---
+++ src/main/java/dedoid/glutio/common/GlutIO.java
@@ -0,0 +1,26 @@
+package dedoid.glutio.common;
+
+import dedoid.glutio.common.lib.LibMisc;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.VERSION)
+public class GlutIO {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
---
+++ src/main/java/dedoid/glutio/common/lib/LibMisc.java
@@ -0,0 +1,8 @@
+package dedoid.glutio.common.lib;
+
+public final class LibMisc {
+
+    public static final String MOD_ID = "glutio";
+    public static final String MOD_NAME = "GlutIO";
+    public static final String VERSION = "0.0.1";
+}
